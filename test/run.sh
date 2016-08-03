337143b9cac90cdad229be6115b6bcef7eeb2a14
Dave Syer <dsyer@pivotal.io>
2016-08-03 07:29:23 -0700

    Fix test script to use explicit path to spring

--- test/run.sh
+++ test/run.sh
@@ -1,22 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 cd $(dirname $0)
 
-BO0T_VERSION=1.4.0.RELEASE
+BOOT_VERSION=1.4.0.RELEASE
 
 if ! [ -d "$HOME/.sdkman/bin" ]; then
     curl -s get.sdkman.io | bash
     sleep 10
 fi
-[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-if ! [ -d "$HOME/.sdkman/springboot/$BOOT_VERSION" ]; then
+[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ] && . "$HOME/.sdkman/bin/sdkman-init.sh"
+if ! [ -d "$HOME/.sdkman/candidates/springboot/$BOOT_VERSION" ]; then
     echo "Y" | sdk install springboot $BOOT_VERSION
-    sdk use springboot $BOOT_VERSION
 fi
+sdk use springboot $BOOT_VERSION
 
 cd ../complete
-spring run app.groovy &
-sleep 20
+$HOME/.sdkman/candidates/springboot/$BOOT_VERSION/bin/spring run app.groovy &
+sleep 30
 curl -s http://localhost:8080 > actual.html
 kill `jps | grep JarLauncher | cut -d " " -f 1`
 
