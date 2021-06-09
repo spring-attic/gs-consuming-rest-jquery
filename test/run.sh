c92277c9d577cd3d673cc44eb4725503144d8226
Greg L. Turnquist <gturnquist@vmware.com>
2021-06-09 15:31:15 -0500

    Bake a custom container with Spring CLI.

--- test/run.sh
+++ test/run.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-cd $(dirname $0)
+cd complete
 
-BOOT_VERSION=2.0.0.RELEASE
+echo "Launching Spring+jQuery app in background..."
+/usr/local/bin/spring run app.groovy &
 
-if ! [ -d "$HOME/.sdkman/bin" ]; then
-    curl -s get.sdkman.io | bash
-    sleep 10
-fi
-[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ] && . "$HOME/.sdkman/bin/sdkman-init.sh"
-if ! [ -d "$HOME/.sdkman/candidates/springboot/$BOOT_VERSION" ]; then
-    echo "Y" | sdk install springboot $BOOT_VERSION
-fi
-sdk use springboot $BOOT_VERSION
-
-cd ../complete
-$HOME/.sdkman/candidates/springboot/$BOOT_VERSION/bin/spring run app.groovy &
 sleep 30
+
+echo "Checking if it started in the allotted time..."
 curl -s http://localhost:8080 > actual.html
-kill `jps | grep JarLauncher | cut -d " " -f 1`
 
 echo "Let's look at the actual results: `cat actual.html`"
 echo "And compare it to: `cat ../test/expected.html`"
